c5927b6f1fbb9f96701021e721e3ddf48f04b95a
tribhuvan <ds14041998@gmail.com>
2020-12-09 01:02:20 +0530

    changed fun

--- src/index.js
+++ src/index.js
@@ -25,70 +25,61 @@ function checkForException(num1, num2, res) {
   if (num1 >= 1000000 || num2 >= 1000000) {
     return res.send("Overflow");
   }
-}
-
-app.post("/add", (req, res) => {
-  const num1 = req.body.num1;
-  const num2 = req.body.num2;
-
-  checkForException(num1, num2, res);
-
-  res.send(
-    JSON.stringify({
-      status: "success" | "failure" | "error",
-      message: "the sum of given two numbers",
-      sum: num1 + num2,
-    })
-  );
-});
-
-app.post("/sub", (req, res) => {
-  const num1 = req.body.num1;
-  const num2 = req.body.num2;
-
-  checkForException(num1, num2, res);
-
-  res.send(
-    JSON.stringify({
-      status: "success" | "failure" | "error",
-      message: "the sum of given two numbers",
-      difference: num1 - num2,
-    })
-  );
-});
-
-app.post("/multiply", (req, res) => {
-  const num1 = req.body.num1;
-  const num2 = req.body.num2;
-
-  checkForException(num1, num2, res);
-
-  res.send(
-    JSON.stringify({
-      status: "success" | "failure" | "error",
-      message: "the sum of given two numbers",
-      result: num1 * num2,
-    })
-  );
-});
-
-app.post("/divide", (req, res) => {
-  const num1 = req.body.num1;
-  const num2 = req.body.num2;
-
-  checkForException(num1, num2, res);
-
   if (num2 === 0) {
     return res.send("Cannot divide by zero");
   }
+}
 
-  res.send(
-    JSON.stringify({
-      status: "success" | "failure" | "error",
-      message: "the sum of given two numbers",
-      result: num1 / num2,
-    })
-  );
+app.post("/add", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  checkForException(num1, num2, res);
+
+  res.json({
+    status: "success" | "failure" | "error",
+    message: "the sum of given two numbers",
+    sum: num1 + num2,
+  });
+});
+
+app.post("/sub", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  checkForException(num1, num2, res);
+
+  res.json({
+    status: "success" | "failure" | "error",
+    message: "the difference of given two numbers",
+    difference: num1 - num2,
+  });
+});
+
+app.post("/multiply", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  checkForException(num1, num2, res);
+
+  res.json({
+    status: "success" | "failure" | "error",
+    message: "The product of given numbers",
+    result: num1 * num2,
+  });
+});
+
+app.post("/divide", (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  checkForException(num1, num2, res);
+
+  res.json({
+    status: "success" | "failure" | "error",
+    message: "The division of given numbers",
+    result: num1 / num2,
+  });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
