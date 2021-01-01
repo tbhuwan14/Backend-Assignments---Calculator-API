2ae478d87c4c47a36a8b35c58a5c31fbba005cbc
tribhuvan <ds14041998@gmail.com>
2021-01-01 21:04:14 +0530

    sd

--- src/index.js
+++ src/index.js
@@ -11,24 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 // your code goes here
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.post("/add", (req, res) => {
   const num1 = req.body.num1;
   const num2 = req.body.num2;
 
   if (typeof num1 === "string" || typeof num2 === "string") {
-    return res
-      .status(400)
-      .json({ status: "error", message: "Invalid data types" });
+    return res.send({ status: "error", message: "Invalid data types" });
   } else if (num1 < -1000000 || num2 < -1000000 || num1 + num2 < -1000000) {
-    return res.status(400).json({ status: "error", message: "Underflow" });
+    return res.send({ status: "error", message: "Underflow" });
   } else if (num1 > 1000000 || num2 > 1000000 || num1 + num2 > 1000000) {
-    return res.status(400).json({ status: "error", message: "Overflow" });
+    return res.send({ status: "error", message: "Overflow" });
   } else {
-    res.json({
+    res.send({
       status: "success",
       message: "the sum of given two numbers",
       sum: num1 + num2,
@@ -41,15 +36,13 @@ app.post("/sub", (req, res) => {
   const num2 = req.body.num2;
 
   if (typeof num1 === "string" || typeof num2 === "string") {
-    return res
-      .status(400)
-      .json({ status: "error", message: "Invalid data types" });
+    return res.send({ status: "error", message: "Invalid data types" });
   } else if (num1 < -1000000 || num2 < -1000000 || num1 - num2 < -1000000) {
-    return res.status(400).json({ status: "error", message: "Underflow" });
+    return res.send({ status: "error", message: "Underflow" });
   } else if (num1 > 1000000 || num2 > 1000000 || num1 - num2 > 1000000) {
-    return res.status(400).json({ status: "error", message: "Overflow" });
+    return res.send({ status: "error", message: "Overflow" });
   } else
-    res.json({
+    res.send({
       status: "success",
       message: "the difference of given two numbers",
       difference: num1 - num2,
@@ -61,15 +54,13 @@ app.post("/multiply", (req, res) => {
   const num2 = req.body.num2;
 
   if (typeof num1 === "string" || typeof num2 === "string") {
-    return res
-      .status(400)
-      .json({ status: "error", message: "Invalid data types" });
+    return res.send({ status: "error", message: "Invalid data types" });
   } else if (num1 < -1000000 || num2 < -1000000 || num1 * num2 < -1000000) {
-    return res.status(400).json({ status: "error", message: "Underflow" });
+    return res.send({ status: "error", message: "Underflow" });
   } else if (num1 > 1000000 || num2 > 1000000 || num1 * num2 > 1000000) {
-    return res.status(400).json({ status: "error", message: "Overflow" });
+    return res.send({ status: "error", message: "Overflow" });
   } else
-    res.json({
+    res.send({
       status: "success",
       message: "The product of given numbers",
       result: num1 * num2,
@@ -81,19 +72,15 @@ app.post("/divide", (req, res) => {
   const num2 = req.body.num2;
 
   if (typeof num1 === "string" || typeof num2 === "string") {
-    return res
-      .status(400)
-      .json({ status: "error", maessage: "Invalid data types" });
+    return res.send({ status: "error", maessage: "Invalid data types" });
   } else if (num1 < -1000000 || num2 < -1000000) {
-    return res.status(400).json({ status: "error", message: "Underflow" });
+    return res.send({ status: "error", message: "Underflow" });
   } else if (num1 > 1000000 || num2 > 1000000) {
-    return res.status(400).json({ status: "error", message: "Overflow" });
+    return res.send({ status: "error", message: "Overflow" });
   } else if (num2 === 0) {
-    return res
-      .status(400)
-      .json({ status: "error", message: "Cannot divide by zero" });
+    return res.send({ status: "error", message: "Cannot divide by zero" });
   } else
-    res.json({
+    res.send({
       status: "success",
       message: "The division of given numbers",
       result: num1 / num2,
